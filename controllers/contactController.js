d95fd7c36bb8d6db4b0844adf9fffbbec9fd9582
dynamitedeeps <sonkhladeepanshu@gmail.com>
2024-02-08 00:45:11 +0530

    authentication with user id

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -48,6 +48,11 @@ const updateContact = asyncHandler(async (req,res)=>{
 		res.status(404);
 		throw new Error("Contact not found");
 	}
+
+	if(contact.user_id.toString() !== req.user.id){
+		res.status(403);
+		throw new Error("User don't have permission to update other user contacts")
+	}
 	const update = await Contact.findByIdAndUpdate(
 		req.params.id,
 		req.body,
@@ -65,6 +70,12 @@ const deleteContact = asyncHandler(async (req,res)=>{
 		res.status(404);
 		throw new Error("Contact not found");
 	}
+
+	if(contact.user_id.toString() !== req.user.id){
+		res.status(403);
+		throw new Error("User don't have permission to delete other user contacts")
+	}
+
 	await Contact.findByIdAndDelete(req.params.id);
 	res.status(200).json(contact);
 });
---
+++ controllers/userController.js
@@ -0,0 +1,75 @@
+const asyncHandler = require("express-async-handler");
+const User = require("../models/userModel");
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+//@description Register a user
+//@route POST /api/users/register
+//@access public
+const registerUser =  asyncHandler(async (req,res) => {
+	const {username, email, password } = req.body;
+	if(!username || !email || !password){
+		res.status(400);
+		throw new Error("All fields are mandatory");
+	}
+	const userAvailable = await User.findOne({email});
+
+	if(userAvailable){
+		res.status(400);
+		throw new Error("User already registered!");
+	}
+	//hash password
+	const hashedPassword = await bcrypt.hash(password,10);
+	
+	const user = await User.create({
+		username,
+		email,
+		password:hashedPassword,
+	})
+	if(user){
+		res.status(201).json({_id:user.id,email:user.email});
+	}else{
+		res.status(400);
+		throw new Error("User data is not valid");
+	}
+});
+
+//@description Login a user
+//@route POST /api/users/login
+//@access public
+const loginUser =  asyncHandler(async (req,res) => {
+	const { email, password} = req.body;
+	if(!email || !password){
+		res.status(400);
+		throw new Error("All field are mandatory!");
+	}
+	const user = await User.findOne({ email });
+
+	//compare password with hashed password
+	if(user && (await bcrypt.compare(password, user.password))){
+		const accessToken = jwt.sign({
+			user: {
+					username: user.username,
+					email: user.email,
+					id: user.id,
+				}
+			}, 
+			process.env.ACCESS_TOKEN_SECRET,
+			{expiresIn:"10m"}
+		);
+		res.status(200).json({accessToken});
+	}
+	else{
+		res.status(400);
+		throw new Error("Email or password is not valid")
+	}
+	res.json({message:"Login the user"});
+});
+
+//@description Current user
+//@route Get /api/users/current
+//@access private
+const currentUser =  asyncHandler(async (req,res) => {
+	res.json(req.user);
+});
+
+module.exports = { registerUser , loginUser, currentUser};
